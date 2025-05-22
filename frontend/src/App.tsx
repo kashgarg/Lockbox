295ddc2ddc2a713255ec929d293fb5b0af005de9
kashgarg <kashishgarg247@gmail.com>
2025-05-22 16:36:03 -0700

    bootstrap React and TypeScript frontend

---
+++ frontend/src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <h1>🔐 LockBox</h1>
+      <p>Secure vault built with Go and React</p>
+    </div>
+  );
+}
+
+export default App;
